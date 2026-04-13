6b93a8004d3c3fa0e317c66c4d64649498a93b17
Teoooooooooo <nikto.nijaky8@gmail.com>
2026-04-13 11:44:09 +0200

    a

---
+++ prvaci/funkcie.py
@@ -0,0 +1,8 @@
+def funkcia(cislo:int):
+    print("Toto je funkcia",cislo)
+    print("Toto je funkcia",cislo)
+    print("Toto je funkcia",cislo)
+    print("Toto je funkcia",cislo)
+    print("Toto je funkcia",cislo)
+
+funkcia(10)
--- prvaci/main.py
+++ prvaci/main.py
@@ -8,7 +8,10 @@ app: FastAPI = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello world"}
+    return {
+        "message": "Hello world",
+        "nova": "Toto je nova spava",
+        }
 
 
 @app.get("/test/1")
@@ -19,6 +22,9 @@ def test():
 def test1():
     return {"message": "Hahahah hehehe"}
 
+@app.get("/teovacesta")
+def teo():
+    return {"pi" : "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505"}
 
 @app.get("/user/{user_id}")
 def get_user(user_id: int) -> dict[str, Any]:
@@ -38,3 +44,4 @@ if __name__ == "__main__":
         port=8000,
         reload=True,
     )
+
