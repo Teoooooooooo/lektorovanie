754911d35376d17b95be5d15b89168b3d9e47c75
Teoooooooooo <nikto.nijaky8@gmail.com>
2026-04-20 11:16:22 +0200

    Update main.py

--- prvaci/main.py
+++ prvaci/main.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
 
 app: FastAPI = FastAPI()
@@ -19,6 +19,12 @@ def test():
 def test1():
     return {"message": "Hahahah hehehe"}
 
+<<<<<<< Updated upstream
+=======
+@app.get("/teovacesta")
+def teo():
+    return {"pi" : "3.1415926535897932384626"}
+>>>>>>> Stashed changes
 
 @app.get("/user/{user_id}")
 def get_user(user_id: int) -> dict[str, Any]:
@@ -29,12 +35,58 @@ def get_user(user_id: int) -> dict[str, Any]:
         }
     except Exception as e:
         return {"error": str(e)}
+    
+@app.post("/echo")
+async def echo(request: Request):
+    body = await request.json()
+    return {"my_answer": body["input"],
+            "moje_mone": body["meno"]}
+
+@app.post("{/scitanie")
+async def scitanie(request: Request):
+    body = await request.json()
+    return {"sucet": body["cislo1"] + body["cislo2"]}
+
+@app.post("/odcitanie")
+async def odcitanie(request: Request):
+    body = await request.json()
+    return {"sucet": body["cislo1"] - body["cislo2"]}
+
+@app.post("/nasobenie")
+async def nasobenie(request: Request):
+    body = await request.json()
+    return {"sucet": body["cislo1"] * body["cislo2"]}
+
+@app.post("/delenie")
+async def delenie(request: Request):
+    body = await request.json()
+    return {"sucet": body["cislo1"] / body["cislo2"]}
+
+
+@app.get("/scitanie/{cislo1}/{cislo2}")
+async def scitanie(cislo1: int,cislo2: int):
+    return {"sucet": cislo1 + cislo2}
+
+@app.post("/spojenie")
+async def spojenie(request: Request):
+    body = await request.json()
+    return {"vysledok": body["meno"] + " " + body["priezvysko"]}
+    
+@app.post("/login")
+async def login(request: Request):
+    body = await request.json()
+    if(body["username"] == "admin" and body["password"] == "heslo"):
+        return {"message" : "login succesfull"}
+    else:
+        return {"message" : "login failed"}
+        
+    
 
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="127.0.0.1",
-        port=8000,
+        port=8005,
         reload=True,
     )
